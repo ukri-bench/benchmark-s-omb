9db22fa31eb3e4d23133680ecd2c49289a1fdf50
Brian Austin <baustin@lbl.gov>
2023-06-15 07:45:32 -0700

    debugging build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-omb_n10=$(pwd)
+omb_n10=`pwd`
 omb_version=osu-micro-benchmarks-7.1-1
 omb_site=https://mvapich.cse.ohio-state.edu/download/mvapich
 
-wget $omb_site/$omb_version.tar.gz
+#wget $omb_site/$omb_version.tar.gz
 tar -xzf $omb_version.tar.gz
 cd $omb_version
 
 ./configure \
   CC=cc  \
   CXX=CC \
-  --prefix=$(omb_n10)/osu-micro-benchmarks-7.1-1-install
+  --prefix=$omb_n10 \
   --enable-cuda=basic \
   --with-cuda=$CUDA_HOME
 
-make
-make install
+  make
+  make install
+
+
