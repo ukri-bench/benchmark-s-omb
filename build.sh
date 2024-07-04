d5bdf83f9b570a0933d54d33ab7d219354648b4c
Andy Turner <a.turner@epcc.ed.ac.uk>
2024-07-04 08:20:40 +0100

    Updates build script for ARCHER2

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-omb_n10=`pwd`
+omb_prefix=$(pwd)
 omb_version=osu-micro-benchmarks-7.1-1
 omb_site=https://mvapich.cse.ohio-state.edu/download/mvapich
 
-#wget $omb_site/$omb_version.tar.gz
+wget $omb_site/$omb_version.tar.gz
 tar -xzf $omb_version.tar.gz
 cd $omb_version
+
+module load PrgEnv-gnu
 
 ./configure \
   CC=cc  \
   CXX=CC \
-  --prefix=$omb_n10 \
-  --enable-cuda=basic \
-  --with-cuda=$CUDA_HOME
+  --prefix=$omb_prefix \
 
   make
   make install
